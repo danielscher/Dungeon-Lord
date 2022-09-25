7a74796fbc017be6e49741039d511a4835bf709b
Keen <qiaokiefer@gmail.com>
2022-09-25 18:27:08 +0200

    adding config,bidding square,gamedata class, and the phase structure, subclass for phase are empty now

---
+++ src/main/java/de/unisaarland/cs/se/selab/Config.java
@@ -0,0 +1,35 @@
+package de.unisaarland.cs.se.selab;
+import de.unisaarland.cs.se.selab.comm.ServerConnection;
+
+import java.util.*;
+public class Config {
+    private String configFilePath;
+    private int maxPlayer, maxYear;
+    private LinkedList<Monster> monsters;
+    private LinkedList<Adventurer> adventurers;
+    private LinkedList<Trap> traps;
+    private LinkedList<Room> rooms;
+
+    private boolean parse(){
+        return false;
+    }
+
+    private boolean checkIfValid(){
+        return false;
+    }
+
+    private Monster createMonster(/*...need parameter*/){
+        return NULL;
+    }
+
+    private Adventurer createAdventurer(/*...need parameter*/){
+        return NULL;
+    }
+
+    private void shuffle(){}
+
+    public List<Adventurer> drawAdventurers( int amount){
+        return NULL;
+    }
+// i think we still need getter to pass the data to GameData class.
+}
