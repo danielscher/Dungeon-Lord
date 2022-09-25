bfedae83cdd1501d7eac5bc56812eadbd970f02a
Can Wang <cawa00001@stud.uni-saarland.de>
2022-09-25 21:43:38 +0200

    test changing personal Username and email, skeletion of the CollectAndPlaceBidPhase

--- src/main/java/de/unisaarland/cs/se/selab/phase/ChooseBattleGroundPhase.java
+++ src/main/java/de/unisaarland/cs/se/selab/phase/ChooseBattleGroundPhase.java
@@ -1,4 +1,34 @@
 package de.unisaarland.cs.se.selab.phase;
 
 public class ChooseBattleGroundPhase {
+    public Phase run(){
+        return NULL;
+    }
+    private void eval(){
+        //to_do
+        //iterates over Bidding Square
+    }
+
+    private void grant(Player p,int r, int c){
+        //to_do
+        //grants a single bid
+    }
+
+    private void exec(PlaceBidAction pba){
+        //to_do
+    }
+
+    private void exec(ActivateRoomAction ara){
+        //to_do
+    }
+
+    private boolean checkIfAllBidsChosen(){
+        //to_do
+        return false;
+    }
+
+    private void blockBids(){
+        //to_to
+    }
+
 }
