693362afb236abcae9289ac930695ff7a9f1ce2c
Can Wang <cawa00001@stud.uni-saarland.de>
2022-09-26 20:08:01 +0200

    delete weird lines in config file

--- src/main/java/de/unisaarland/cs/se/selab/game/Config.java
+++ src/main/java/de/unisaarland/cs/se/selab/game/Config.java
@@ -1,16 +1,13 @@
 package de.unisaarland.cs.se.selab.game;
-<<<<<<< HEAD
-=======
+
 
 import de.unisaarland.cs.se.selab.comm.ServerConnection;
->>>>>>> c62f79381689a371ce5299546e55d2edbef794f4
-
 import de.unisaarland.cs.se.selab.game.entities.Adventurer;
 import de.unisaarland.cs.se.selab.game.entities.Attack;
 import de.unisaarland.cs.se.selab.game.entities.Monster;
 import de.unisaarland.cs.se.selab.game.entities.Room;
 import de.unisaarland.cs.se.selab.game.entities.Trap;
-<<<<<<< HEAD
+
 import de.unisaarland.cs.se.selab.game.util.Location;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -21,187 +18,176 @@ import java.util.Set;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-=======
+
 import java.util.*;
->>>>>>> c62f79381689a371ce5299546e55d2edbef794f4
+
 
 public class Config {
 
     private String configFilePath;
-    private int maxPlayer =-1;
-    private int maxYear=-1;
-    private int dungeonSidelength =-1;
+    private int maxPlayer = -1;
+    private int maxYear = -1;
+    private int dungeonSidelength = -1;
     private int initFood = -1;
     private int initGold = -1;
-    private int  initImps= -1;
+    private int initImps = -1;
     private boolean parserResult, configResult;
     private ArrayList<Monster> monsters;
     private ArrayList<Adventurer> adventurers;
     private ArrayList<Trap> traps;
     private ArrayList<Room> rooms;
 
-<<<<<<< HEAD
-    private boolean parse(){
-    parserResult=true;
-    JSONObject obj= new JSONObject(".\\resources\\configuration.json");
-    maxYear = (Integer)obj.get("years");
-    maxPlayer = (Integer)obj.get("maxPlayers");
-    dungeonSidelength = (Integer)obj.get("dungeonSidelength");
-    initFood = (Integer)obj.get("initialFood");
-    initGold = (Integer)obj.get("initialGold");
-    initImps = (Integer)obj.get("initialImps");
 
-    JSONArray monsterArray = obj.getJSONArray("monsters");
-    int mALength =monsterArray.length();
-    for(int i=0;i<mALength;i++){
-        JSONObject monsterObj = monsterArray.getJSONObject(i);
-        int id = (Integer)monsterObj.get("id");
-        String name = (String) monsterObj.get("name");
-        int hunger = (Integer)monsterObj.get("hunger");
-        int evilness = (Integer)monsterObj.get("evilness");
-        int damage = (Integer)monsterObj.get("damage");
-        Attack attackStrategy = (Attack) monsterObj.get("attackStrategy");
-        if(evilness<0||hunger<0){
-            parserResult=false;
-            break;
-        }
-        // invalid evil/hunger
+    private boolean parse() {
+        parserResult = true;
+        JSONObject obj = new JSONObject(".\\resources\\configuration.json");
+        maxYear = (Integer) obj.get("years");
+        maxPlayer = (Integer) obj.get("maxPlayers");
+        dungeonSidelength = (Integer) obj.get("dungeonSidelength");
+        initFood = (Integer) obj.get("initialFood");
+        initGold = (Integer) obj.get("initialGold");
+        initImps = (Integer) obj.get("initialImps");
 
-        if(damage<1||attackStrategy==null){
-            parserResult=false;
-            break;
+        JSONArray monsterArray = obj.getJSONArray("monsters");
+        int mALength = monsterArray.length();
+        for (int i = 0; i < mALength; i++) {
+            JSONObject monsterObj = monsterArray.getJSONObject(i);
+            int id = (Integer) monsterObj.get("id");
+            String name = (String) monsterObj.get("name");
+            int hunger = (Integer) monsterObj.get("hunger");
+            int evilness = (Integer) monsterObj.get("evilness");
+            int damage = (Integer) monsterObj.get("damage");
+            Attack attackStrategy = (Attack) monsterObj.get("attackStrategy");
+            if (evilness < 0 || hunger < 0) {
+                parserResult = false;
+                break;
+            }
+            // invalid evil/hunger
+
+            if (damage < 1 || attackStrategy == null) {
+                parserResult = false;
+                break;
+            }
+            // valid damage, attack not set
         }
-        // valid damage, attack not set
+
+        JSONArray adventurerArray = obj.getJSONArray("adventurers");
+        int aALength = adventurerArray.length();
+        for (int i = 0; i < aALength; i++) {
+            JSONObject adventurerObj = adventurerArray.getJSONObject(i);
+            int id = (Integer) adventurerObj.get("id");
+            String name = (String) adventurerObj.get("name");
+            int difficulty = (Integer) adventurerObj.get("difficulty");
+            int healthPoints = (Integer) adventurerObj.get("healthPoints");
+            int healValue = (Integer) adventurerObj.get("healValue");
+            int defuseValue = (Integer) adventurerObj.get("defuseValue");
+            boolean charge = (boolean) adventurerObj.get("charge");
+            if (difficulty < 0 || healthPoints < 1 || healValue < 0 || defuseValue < 0) {
+                parserResult = false;
+                break;
+            }
+            // invalid value of an adventurer
+
+            if (healValue > 0 && defuseValue > 0) {
+                parserResult = false;
+                break;
+            }
+            // cannot be priest and thief at the same time
+        }
+
+        JSONArray trapArray = obj.getJSONArray("traps");
+        int tALength = trapArray.length();
+        for (int i = 0; i < tALength; i++) {
+            JSONObject trapObj = trapArray.getJSONObject(i);
+            int id = (Integer) trapObj.get("id");
+            String name = (String) trapObj.get("name");
+            int damage = (Integer) trapObj.get("damage");
+            Attack attackStrategy = (Attack) trapObj.get("attackStrategy");
+            int target = (Integer) trapObj.get("target");
+            if (id < 0 || damage < 1) {
+                parserResult = false;
+                break;
+            }
+            //traps should have valid damage and id
+            if (attackStrategy == Attack.TARGETED && (target < 1 || target > 3)) {
+                parserResult = false;
+                break;
+            }
+            //target traps should have 1,2,3 as goals.
+        }
+
+        JSONArray roomArray = obj.getJSONArray("rooms");
+        int rALength = roomArray.length();
+        for (int i = 0; i < rALength; i++) {
+            JSONObject roomObj = roomArray.getJSONObject(i);
+            int id = (Integer) roomObj.get("id");
+            String name = (String) roomObj.get("name");
+            int activationCost = (Integer) roomObj.get("activation");
+            Location placementLoc = (Location) roomObj.get("restriction");
+            int foodProduction = (Integer) roomObj.get("food");
+            int goldProduction = (Integer) roomObj.get("gold");
+            int impProduction = (Integer) roomObj.get("imps");
+            int niceness = (Integer) roomObj.get("niceness");
+            if (id < 0) {
+                parserResult = false;
+                break;
+            }
+        }
+
+        return parserResult;
     }
 
-    JSONArray adventurerArray = obj.getJSONArray("adventurers");
-    int aALength =adventurerArray.length();
-    for(int i=0;i<aALength;i++){
-        JSONObject adventurerObj = adventurerArray.getJSONObject(i);
-        int id= (Integer)adventurerObj.get("id");
-        String name = (String) adventurerObj.get("name");
-        int difficulty = (Integer)adventurerObj.get("difficulty");
-        int healthPoints = (Integer)adventurerObj.get("healthPoints");
-        int healValue = (Integer)adventurerObj.get("healValue");
-        int defuseValue = (Integer)adventurerObj.get("defuseValue");
-        boolean charge = (boolean) adventurerObj.get("charge");
-        if(difficulty<0||healthPoints<1||healValue<0||defuseValue<0){
-            parserResult=false;
-            break;
-        }
-        // invalid value of an adventurer
 
-        if(healValue>0 &&defuseValue >0){
-            parserResult=false;
-            break;
-        }
-        // cannot be priest and thief at the same time
-    }
-
-    JSONArray trapArray = obj.getJSONArray("traps");
-    int tALength = trapArray.length();
-    for(int i=0;i<tALength;i++){
-        JSONObject trapObj = trapArray.getJSONObject(i);
-        int id = (Integer)trapObj.get("id");
-        String name = (String) trapObj.get("name");
-        int damage = (Integer)trapObj.get("damage");
-        Attack attackStrategy = (Attack) trapObj.get("attackStrategy");
-        int target = (Integer)trapObj.get("target");
-        if(id < 0 ||damage<1){
-            parserResult = false;
-            break;
-        }
-        if(attackStrategy==Attack.TARGETED &&(target<1||target>3)){
-            parserResult = false;
-            break;
-        }
-    }
-
-    JSONArray roomArray = obj.getJSONArray("rooms");
-    int rALength =roomArray.length();
-    for(int i=0;i<rALength;i++){
-        JSONObject roomObj = roomArray.getJSONObject(i);
-        int id = (Integer)roomObj.get("id");
-        String name = (String) roomObj.get("name");
-        int activationCost = (Integer)roomObj.get("activation");
-        Location placementLoc = (Location) roomObj.get("restriction");
-        int foodProduction = (Integer)roomObj.get("food");
-        int goldProduction = (Integer)roomObj.get("gold");
-        int impProduction = (Integer)roomObj.get("imps");
-        int niceness = (Integer)roomObj.get("niceness");
-        if(id < 0){
-            parserResult = false;
-            break;
-        }
-    }
-
-    return parserResult;
-    }
-
-
-    private boolean checkIfValid(){
-        configResult=true;
-        if(maxYear<1 ||maxPlayer<1){
-            configResult=false;
+    private boolean checkIfValid() {
+        configResult = true;
+        if (maxYear < 1 || maxPlayer < 1) {
+            configResult = false;
             //not enough years/player,
         }
-        if(traps.size()<4*4*maxYear){
-            configResult=false;
+        if (traps.size() < 4 * 4 * maxYear) {
+            configResult = false;
             //not enough traps
         }
 
-        if(rooms.size()<2*4*maxYear){
-            configResult=false;
+        if (rooms.size() < 2 * 4 * maxYear) {
+            configResult = false;
             //not enough rooms
         }
 
-        if(monsters.size()<3*4*maxYear){
-            configResult=false;
+        if (monsters.size() < 3 * 4 * maxYear) {
+            configResult = false;
             //not enough monsters
         }
-        if(adventurers.size()<3*3*maxPlayer){
-            configResult=false;
+        if (adventurers.size() < 3 * 3 * maxPlayer) {
+            configResult = false;
             //not enough rooms
         }
 
-        if(dungeonSidelength<1 || dungeonSidelength>15){
-            configResult=false;
+        if (dungeonSidelength < 1 || dungeonSidelength > 15) {
+            configResult = false;
             // dungeon too small or big
         }
-    // !!! try to check the duplicate elements, but get error...
-    //   HashSet<Trap> checkTraps = new HashSet<>(Arrays.asList(traps));
+        // !!! try to check the duplicate elements, but get error...
+        //   HashSet<Trap> checkTraps = new HashSet<>(Arrays.asList(traps));
 
         return configResult;
-=======
-    private boolean parse() {
-        return false;
     }
 
-    private boolean checkIfValid() {
-        return false;
->>>>>>> c62f79381689a371ce5299546e55d2edbef794f4
-    }
+        private Monster createMonster (/*...need parameter*/) {
+            // return NULL;
+            return null;
+        }
 
-    private Monster createMonster(/*...need parameter*/) {
+        private Adventurer createAdventurer (/*...need parameter*/) {
+            return null;
+        }
 
-<<<<<<< HEAD
-        return null;
-=======
-        // return NULL;
->>>>>>> c62f79381689a371ce5299546e55d2edbef794f4
-    }
+        private void shuffle () {
+        }
 
-    private Adventurer createAdventurer(/*...need parameter*/) {
+        public List<Adventurer> drawAdventurers ( int amount){
 
-        return null;
-    }
-
-    private void shuffle() {
-    }
-
-    public List<Adventurer> drawAdventurers(int amount) {
-
-        return null;
-    }
+            return null;
+        }
 // i think we still need getter to pass the data to GameData class.
-}
+    }
---
+++ src/test/java/ConfigTest.java
@@ -0,0 +1,5 @@
+import de.unisaarland.cs.se.selab.game.Config;
+
+public class ConfigTest extends Config {
+
+}
