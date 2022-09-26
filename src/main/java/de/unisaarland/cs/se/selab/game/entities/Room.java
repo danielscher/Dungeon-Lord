b7c7cc7a8307552c62ac666e6b17c9865a531153
Henry Hansen <heha00002@stud.uni-saarland.de>
2022-09-26 13:57:16 +0200

    added constructors setters and getters to entity classes

--- src/main/java/de/unisaarland/cs/se/selab/game/entities/Room.java
+++ src/main/java/de/unisaarland/cs/se/selab/game/entities/Room.java
@@ -4,7 +4,63 @@ import de.unisaarland.cs.se.selab.game.util.Location;
 
 public class Room {
 
-    private int roomID, activationCost, foodProduction, goldProduction, impProduction, niceness;
+    private int roomID;
+    private int activationCost;
+    private int foodProduction;
+    private int goldProduction;
+    private int impProduction;
+    private int niceness;
     private boolean activated;
     private Location placementLoc;
+
+    public Room(int roomID, int activationCost, int foodProduction, int goldProduction, int impProduction, int niceness, Location placementLoc) {
+        this.roomID = roomID;
+        this.activationCost = activationCost;
+        this.foodProduction = foodProduction;
+        this.goldProduction = goldProduction;
+        this.impProduction = impProduction;
+        this.niceness = niceness;
+        this.placementLoc = placementLoc;
+        this.activated = false;
+    }
+
+    public int getRoomID() {
+        return roomID;
+    }
+
+    public int getActivationCost() {
+        return activationCost;
+    }
+
+    public int getFoodProduction() {
+        return foodProduction;
+    }
+
+    public int getGoldProduction() {
+        return goldProduction;
+    }
+
+    public int getImpProduction() {
+        return impProduction;
+    }
+
+    public int getNiceness() {
+        return niceness;
+    }
+
+    public Location getPlacementLoc() {
+        return placementLoc;
+    }
+
+    public boolean isActivated() {
+        return activated;
+    }
+
+    public void deactivate() {
+        activated = false;
+    }
+
+    public void activate() {
+        activated = true;
+    }
 }
--- src/main/java/de/unisaarland/cs/se/selab/game/entities/Trap.java
+++ src/main/java/de/unisaarland/cs/se/selab/game/entities/Trap.java
@@ -7,4 +7,40 @@ public class Trap {
     private int target;
     private Attack attack;
     private boolean availableThisYear;
+
+    public Trap(int trapID, int damage, int target, Attack attack) {
+        this.trapID = trapID;
+        this.damage = damage;
+        this.target = target;
+        this.attack = attack;
+        this.availableThisYear = true;
+    }
+
+    public int getTrapID() {
+        return trapID;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public int getTarget() {
+        return target;
+    }
+
+    public Attack getAttack() {
+        return attack;
+    }
+
+    public boolean isAvailableThisYear() {
+        return availableThisYear;
+    }
+
+    public void setUnavailable() {
+        availableThisYear = false;
+    }
+
+    public void setAvailable() {
+        availableThisYear = true;
+    }
 }
