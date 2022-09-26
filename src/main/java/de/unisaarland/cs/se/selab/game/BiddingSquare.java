99b601b8778309ed8bed54ece93908a6b94ed4c4
harishanth <hasi00001@stud.uni-saarland.de>
2022-09-26 12:37:54 +0200

    completeBiddingSquare

--- src/main/java/de/unisaarland/cs/se/selab/game/BiddingSquare.java
+++ src/main/java/de/unisaarland/cs/se/selab/game/BiddingSquare.java
@@ -1,39 +1,59 @@
 package de.unisaarland.cs.se.selab.game;
 
 import de.unisaarland.cs.se.selab.comm.BidType;
-
+import java.util.HashMap;
 import java.util.Map;
 
 public class BiddingSquare {
-    private int [][] biddingSlots; // row=slot, column=bidtype, [3][8]
-    private Map<BidType,Integer> typeToColumnMap;
-    private Map<Integer,BidType> columnToTypeMap;
 
-    public boolean insert(BidType FOOD, int playerID){
-        return false;
-        // insert playerID to the uppermost free row
+    private final int[][] biddingSlots = new int[3][8];
+    private final Map<BidType, Integer> typeToColumnMap = new HashMap<BidType, Integer>();
+    private final Map<Integer, BidType> columnToTypeMap = new HashMap<Integer, BidType>();
+
+
+    public boolean insert(BidType bidType, int playerID) {
+        // get the uppermost free row
+        int columnId = typeToColumn(bidType);
+        boolean res = false;
+        for (int row = 0; row < biddingSlots.length; row++) {
+            if (biddingSlots[row][columnId] == -1) {
+                biddingSlots[row][columnId] = playerID;
+                res = true;
+                break;
+            }
+
+        }
+        return res;
     }
 
-    public int getIDByBidSlot(int row,int column){
-        return -1;
+    public int getIDByBidSlot(int row, int column) {
+        return biddingSlots[row][column];
     }
 
-    public int getIDByBidSlot(BidType FOOD,int column){
-        return -1;
+    public int getIDByBidSlot(BidType bidType, int row) {
+        int columnId = typeToColumn(bidType);
+        return biddingSlots[row][columnId];
+
     }
 
-    private int typeToColumn(BidType FOOD){
-        return -1;
+    private int typeToColumn(BidType bidType) {
+        return typeToColumnMap.get(bidType);
     }
 
-    private BidType columnToType(int column){
-
-        //return NULL;
-        //test
+    private BidType columnToType(int column) {
+        return columnToTypeMap.get(column);
     }
 
-    public int countTrapBids(){
-        return 0;
+    public int countTrapBids() {
+        int columnId = typeToColumn(BidType.TRAP);
+        int res = 0;
+        for (int row = 0; row < biddingSlots.length; row++) {
+            if (biddingSlots[row][columnId] != -1) {
+                res = res + 1;
+            }
+        }
+        return res;
+
     }
 
 }
