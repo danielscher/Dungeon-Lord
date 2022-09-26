caadb09ce7ec93c1a1cb37e057a2ebcac9f17015
Henry Hansen <heha00002@stud.uni-saarland.de>
2022-09-26 13:39:16 +0200

    fixed bug of setting distance of null tiles

--- src/main/java/de/unisaarland/cs/se/selab/game/player/Dungeon.java
+++ src/main/java/de/unisaarland/cs/se/selab/game/player/Dungeon.java
@@ -103,7 +103,9 @@ public class Dungeon {
     private void clearDistances() {
         for (Tile[] tileRow : grid) {  // for each tile row of grid
             for (Tile tile : tileRow) {  // for each tile of tile row
-                tile.setDistanceToEntrance(-1);  // set distance to default value
+                if(tile != null) {
+                    tile.setDistanceToEntrance(-1);  // set distance to default value
+                }
             }
         }
     }
