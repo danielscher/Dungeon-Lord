b1653fee4e423d9fe2dc3a5f73e773da6a61f12b
Henry Hansen <heha00002@stud.uni-saarland.de>
2022-09-28 14:00:44 +0200

    changed canPlaceRoomOn method according to spec and added overloading

--- src/main/java/de/unisaarland/cs/se/selab/game/player/Dungeon.java
+++ src/main/java/de/unisaarland/cs/se/selab/game/player/Dungeon.java
@@ -170,6 +170,12 @@ public class Dungeon {
         }
     }
 
+    /*
+    overloading of below method to adapt to usage of coordinate class
+     */
+    public boolean canPlaceRoomOn(Coordinate coordinate, Location location) {
+        return canPlaceRoomOn(coordinate.getxpos(), coordinate.getypos(), location);
+    }
 
     /*
     checks if a room can be placed on a given coordinate
@@ -203,10 +209,10 @@ public class Dungeon {
                 return (y > grid[0].length / 2 - 1);
             }
             case INNER_RING: {
-                return ((x != 0 || x != grid.length - 1) && (y != 0 || y != grid[0].length - 1));
+                return ((x > 0 && x < grid.length - 1) && (y > 0 && y < grid[0].length - 1));
             }
             case OUTER_RING: {
-                return ((x == 0 || x == grid.length - 1) && (y == 0 || y == grid[0].length - 1));
+                return !((x > 0 && x < grid.length - 1) && (y > 0 && y < grid[0].length - 1));
             }
             default:
                 return false;
