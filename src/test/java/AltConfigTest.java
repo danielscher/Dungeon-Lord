af06bc63926e42faab3e9dfee7d13e3a70df33fa
Henry Hansen <heha00002@stud.uni-saarland.de>
2022-09-29 00:57:34 +0200

    added a test class for AltConfig

---
+++ src/test/java/AltConfigTest.java
@@ -0,0 +1,22 @@
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import de.unisaarland.cs.se.selab.game.AltConfig;
+import de.unisaarland.cs.se.selab.game.Config;
+import org.junit.jupiter.api.Test;
+
+public class AltConfigTest extends Config {
+
+    private String mypath = "";
+    AltConfig config = new AltConfig(mypath);
+
+    @Test
+    public void testParse() {
+        try {
+            boolean parsedSuccessfully = config.parse();
+            assertTrue(parsedSuccessfully, "config validation failed");
+        } catch (Exception e) {
+            assertTrue(false, "parsing failed" + e.toString());
+        }
+    }
+
+
+}
