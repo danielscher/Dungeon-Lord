e607fcd274d6ea307ede131df031189c0472f156
Yuqing Zhu <s8ygzhuu@stud.uni-saarland.de>
2022-09-29 14:50:11 +0200

    AdventurerTest

---
+++ src/test/java/AdventurerTest.java
@@ -0,0 +1,49 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import de.unisaarland.cs.se.selab.game.entities.Adventurer;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class AdventurerTest {
+    private Adventurer a1  = new Adventurer(1, 1, 2, 1, 1, true);
+
+    @BeforeEach
+    public void resetAdv() {
+        a1 = new Adventurer(1, 1, 2, 1, 1, true);
+    }
+
+    @Test
+    public void testdamage1() {
+        assertEquals(1, a1.damagehealthby(3));
+        assertEquals(0, a1.getHealthPoints());
+    }
+
+    @Test
+    public void testdamage2() {
+        assertEquals(-1, a1.damagehealthby(1));
+        assertEquals(1, a1.getHealthPoints());
+    }
+
+    @Test
+    public void testHeal1() {
+        int x = a1.damagehealthby(2);
+        assertEquals(0, a1.healBy(2));
+        assertEquals(2, a1.getHealthPoints());
+    }
+
+    @Test
+    public void testHeal2() {
+        int x = a1.damagehealthby(2);
+        assertEquals(1, a1.healBy(3));
+        assertEquals(2, a1.getHealthPoints());
+    }
+
+    @Test
+    public void testHeal3() {
+        int x = a1.damagehealthby(2);
+        assertEquals(0, a1.healBy(0));
+        assertEquals(0, a1.getHealthPoints());
+    }
+}
