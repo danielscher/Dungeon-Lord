14c47b2961fef1c100829363da957e0afb2c068d
Can Wang <cawa00001@stud.uni-saarland.de>
2022-09-30 00:34:15 +0200

    more Config tests

--- src/test/java/ConfigTest.java
+++ src/test/java/ConfigTest.java
@@ -4,32 +4,78 @@ import org.junit.jupiter.api.Test;
 
 public class ConfigTest extends Config {
 
-    private final String mypath = "C:\\Users\\forgo\\group35\\src\\main\\resources\\configuration.json";
+    private final String myPath = "C:\\Users\\forgo\\group35\\src\\main\\resources\\configuration.json";
+    private final String myPathFail = "C:\\Users\\forgo\\group35\\src\\main\\resources\\config_broken.json";
     Config cfg = new Config();
 
     @Test
-    public void testParse() {
-    //    cfg.displayLog();
-        cfg.setConfigFilePath(mypath);
+    public void testParseOk() {
+        //    cfg.displayLog();
+        cfg.setConfigFilePath(myPath);
         try {
-            cfg.parse(mypath);
+            cfg.parse(myPath);
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
-    //    cfg.displayLog();
+        //    cfg.displayLog();
+    }
+
+    public void testParseNotOk() {
+        //    cfg.displayLog();
+        cfg.setConfigFilePath(myPathFail);
+        try {
+            cfg.parse(myPathFail);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        //    cfg.displayLog();
+    }
+
+    public void testParseNotOkM1() {
+        //    cfg.displayLog();
+        cfg.setConfigFilePath(myPath);
+        try {
+            cfg.parse(myPath);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        //    cfg.displayLog();
     }
 
     @Test
     public void testgetter() {
-    //    cfg.displayLog();
-        cfg.setConfigFilePath(mypath);
+        //    cfg.displayLog();
+        cfg.setConfigFilePath(myPath);
         try {
-            cfg.parse(mypath);
+            cfg.parse(myPath);
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
         cfg.displayLog();
     }
 
+    @Test
+    public void testConvertAttack() {
+    }
+
+    @Test
+    public void testConvertLocation() {
+    }
+
+    @Test
+    public void testDeleteMonsters() {
+    }
+
+    @Test
+    public void testDeleteAdventurers() {
+    }
+
+    @Test
+    public void testDeleteTraps() {
+    }
+
+    @Test
+    public void testDeleteRooms() {
+    }
 
 }
