3fc70350d463c98e176d9968cfb1712bcf119582
Yuqing Zhu <s8ygzhuu@stud.uni-saarland.de>
2022-09-29 17:04:05 +0200

    fix pmd in tests but still exist

--- src/test/java/BiddingsquareTest.java
+++ src/test/java/BiddingsquareTest.java
@@ -1,53 +1,56 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import de.unisaarland.cs.se.selab.comm.BidType;
 import de.unisaarland.cs.se.selab.game.BiddingSquare;
-import java.util.HashMap;
-import java.util.Map;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class BiddingsquareTest {
+class BiddingsquareTest {
     BiddingSquare bs = new BiddingSquare();
 
-    @BeforeEach
     private void resetBiddingSquare() {
         bs = new BiddingSquare();
-        final boolean a1 = bs.insert(BidType.FOOD, 2);
-        final boolean a2 = bs.insert(BidType.FOOD, 3);
-        final boolean a3 = bs.insert(BidType.FOOD, 4);
-        final boolean a4 = bs.insert(BidType.GOLD, 3);
-        final boolean a5 = bs.insert(BidType.TRAP, 4);
-        final boolean a6 = bs.insert(BidType.TRAP, 2);
+        bs.insert(BidType.FOOD, 2);
+        bs.insert(BidType.FOOD, 3);
+        bs.insert(BidType.FOOD, 4);
+        bs.insert(BidType.GOLD, 3);
+        bs.insert(BidType.TRAP, 4);
+        bs.insert(BidType.TRAP, 2);
         //  biddingslots = {[2,3,-1,-1,-1,4,-1,-1],
         //                  [3,-1,-1,-1,-1,2,-1,-1],
         //                  [4,-1,-1,-1,-1,-1,-1,-1]}
     }
 
     @Test
-    public void testInsert() {
-        assertEquals(false, bs.insert(BidType.FOOD, 3));
-        assertEquals(true, bs.insert(BidType.GOLD, 3));
+    void testInsert() {
+        resetBiddingSquare();
+        assertFalse(bs.insert(BidType.FOOD, 3));
+        assertTrue(bs.insert(BidType.GOLD, 3));
     }
 
     @Test
-    public void testGetIDByBidSlot() {
+    void testGetIDByBidSlot() {
+        resetBiddingSquare();
         assertEquals('2', bs.getIDByBidSlot(0, 0));
         assertEquals('2', bs.getIDByBidSlot(BidType.FOOD, 0));
     }
 
     @Test
-    public void testTypeToColumn() {
+    void testTypeToColumn() {
+        resetBiddingSquare();
         assertEquals(0, bs.typeToColumn(BidType.FOOD));
     }
 
     @Test
-    public void testColumnToType() {
+    void testColumnToType() {
+        resetBiddingSquare();
         assertEquals(BidType.FOOD, bs.columnToType(0));
     }
 
     @Test
-    public void testCountTrapBids() {
+    void testCountTrapBids() {
+        resetBiddingSquare();
         assertEquals(2, bs.countTrapBids());
     }
 }
