63373530c64e83140e779ca303e93b06a39ea551
Daniel Sherbakov <daniel.00010@gmail.com>
2022-10-01 09:38:23 +0200

    fixed checkStyles in Config

--- src/main/java/de/unisaarland/cs/se/selab/game/Config.java
+++ src/main/java/de/unisaarland/cs/se/selab/game/Config.java
@@ -424,7 +424,7 @@ public class Config {
         // TODO
     }
 
-    public int getParserResult(){
+    public int getParserResult() {
         return parserResult;
     }
 
@@ -486,6 +486,7 @@ public class Config {
                 .clear(); // removes this sublist from list.
         return drawnAdventurers;
     }
+
     // get general information from Config files
     public int getMaxPlayers() {
         return maxPlayer;
