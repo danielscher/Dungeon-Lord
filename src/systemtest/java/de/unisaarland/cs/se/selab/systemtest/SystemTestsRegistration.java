be4c0271378b8d2b2d0d0dfe77e6d765de115411
Henry Hansen <heha00002@stud.uni-saarland.de>
2022-10-05 13:59:18 +0200

    registered all new systemtests

--- src/systemtest/java/de/unisaarland/cs/se/selab/systemtest/SystemTestsRegistration.java
+++ src/systemtest/java/de/unisaarland/cs/se/selab/systemtest/SystemTestsRegistration.java
@@ -11,8 +11,17 @@ final class SystemTestsRegistration {
     }
 
     static void registerSystemTests(final SystemTestManager manager) {
-        // manager.registerTest(new RegistrationTest());
-        // manager.registerTest(new OurSystemTestFramework());
+        manager.registerTest(new RegistrationTest());
+        manager.registerTest(new OurSystemTestFramework());
+        manager.registerTest(new UpToGameStarted());
+        manager.registerTest(new UpToPlayer());
+        manager.registerTest(new UpToNextYear());
+        manager.registerTest(new UpToNextRound());
+        manager.registerTest(new UpToAdventurerDrawn());
+        manager.registerTest(new UpToMonsterDrawn());
+        manager.registerTest(new UpToRoomDrawn());
+        manager.registerTest(new UpToBiddingStarted());
+        manager.registerTest(new UpToActNow());
         manager.registerTest(new SystemTestTemplate());
     }
 }
